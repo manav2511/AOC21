95283a2f81cd876106d2b770b159f33ce8623b01
Manav Raghuwanshi <manav2511@gmail.com>
2021-12-22 03:41:51 +0530

    Add day 3 part 1

--- Day03/day03.cpp
+++ Day03/day03.cpp
@@ -10,7 +10,36 @@ const ll MOD = 1e9 + 7;
 const ll INF = 1e9;
 
 void part1() {
-    
+	ifstream inp("input.txt");
+    vector<string> bin;
+
+    string b;
+    while (inp >> b) {
+        bin.push_back(b);
+    }
+
+    int n = bin.size();
+    std::map<int, int> mp0;
+    std::map<int, int> mp1;
+
+    for (int i = 0; i < n; ++i)
+    {
+    	int k = bin[i].size();
+    	for (int j = 0; j < k; ++j)
+    	{
+    		if ((bin[i][j] - '0') == 0) mp0[j]++;
+    		else mp1[j]++;
+    	}
+    }
+
+    string gmrate, eprate;
+    for (int i = 0; i < mp0.size(); ++i)
+    {
+        if (mp0[i] > mp1[i]) { gmrate += "0"; eprate += "1"; }
+        else { gmrate += "1"; eprate += "0"; }
+    }
+
+    cout << stoi(gmrate, nullptr, 2)*stoi(eprate, nullptr, 2) << "\n";
 }
 
 void part2() {
